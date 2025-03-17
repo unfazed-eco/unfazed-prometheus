37c4bf5d5f557814399bdb1e90b7fac9000290a8
xin.zhou <zhouxin576@gmail.com>
2025-03-17 22:54:07 +0800

    FIX prometheus dir

--- tests/prj/entry/settings.py
+++ tests/prj/entry/settings.py
@@ -39,5 +39,5 @@ UNFAZED_PROMETHEUS_SETTINGS = {
     "HOSTNAME": socket.gethostname(),
     "PROJECT": "unfazed_prometheus",
     "CLIENT_CLASS": "unfazed_prometheus.settings.PrometheusSettings",
-    "PROMETHEUS_MULTIPROC_DIR": "/prometheus",
+    "PROMETHEUS_MULTIPROC_DIR": os.getenv("PROMETHEUS_MULTIPROC_DIR", "/prometheus"),
 }
